c64e5c622cb9088a0f699d0208729f95f2f7a0e0
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-08 20:14:13 +0300

    chore: Add getAllCurrencies function to API module

--- src/component/api.js
+++ src/component/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Get All Currencies from API with vsCurrency as parameter (usd, eur, etc) and return the data as JSON object 
 const getAllCurrencies = async (vsCurrency) => {
   const apiLink = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vsCurrency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
   const response = await axios.get(apiLink);
